e564790cf132137c6e3d6f6c72a75b5fbf2c22f7
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-30 15:40:10 +0530

    added SES

--- hiru3009_pythonproject/newpy.py
+++ hiru3009_pythonproject/newpy.py
@@ -1,21 +1,31 @@
 import boto3
+ses = boto3.client("ses")
 sns = boto3.client("sns")
 
 def handler(event, context):
     
-    data = sns.get_topic_attributes(
-        TopicArn='arn:aws:sns:us-east-1:318300609668:hirutest'
-    )
-    print(data)
-
     try:
-        data = sns.unsubscribe(
-            SubscriptionArn='arn:aws:sns:us-east-1:318300609668:hirutest:jj'
+        data = ses.send_email(
+    		Destination={
+    		    "ToAddresses": ['hirudineel@gmail.com'],
+    			"CcAddresses": [],
+    			"BccAddresses": []
+    		},
+    		Message={
+    			"Body": {
+    			    "Text": {
+    					"Data": 'Sample body'
+    				}
+    			},
+    			"Subject": { 
+    				"Data": 'This is a test'
+    			}
+    		},
+    		Source='hirudinee@adroitlogic.com'
         )
     except BaseException as e:
         print(e)
         raise(e)
-
-    
+  
 
     return {"message": "Successfully executed"}

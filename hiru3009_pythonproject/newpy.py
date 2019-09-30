6c3e5ad0cbb2477b01bfe6193970264e82e9639b
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-30 13:40:51 +0530

    added SNS

---
+++ hiru3009_pythonproject/newpy.py
@@ -0,0 +1,14 @@
+import boto3
+sns = boto3.client("sns")
+
+def handler(event, context):
+    try:
+        data = sns.get_topic_attributes(
+            TopicArn='arn:aws:sns:us-east-1:318300609668:hirutest'
+        )
+        print(data)
+    except BaseException as e:
+        print(e)
+        raise(e)
+    
+    return {"message": "Successfully executed"}
